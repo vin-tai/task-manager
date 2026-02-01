9fe8bd6cd898c5d4389d36da90a65f3fc4a930ff
Vincent Tai <256298459+vin-tai@users.noreply.github.com>
2026-02-01 22:16:09 +0800

    Initial commit

---
+++ src/test/java/com/github/vince_tai/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vince_tai.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
